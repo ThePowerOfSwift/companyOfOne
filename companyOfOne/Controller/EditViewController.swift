e97c69b2f72f6d461b67aa5f22e8f39d38d205fe
Jamie Feeny <feeny.jamie@gmail.com>
2018-10-07 13:05:27 -0700

    added the pickers and the labels to the main doc edit page

---
+++ companyOfOne/Controller/EditViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EditViewController.swift
+//  companyOfOne
+//
+//  Created by Jamie on 2018-10-07.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setupDocTitleTagLabel(){
+        //width is 0.5 times the view width
+    }
+    func setupSubCategoryPicker(){
+        
+    }
+    func setupCategoryPicker(){
+        
+    }
+    func setupReoccurancePicker(){
+        
+    }
+    func setupDocDatePicker(){
+        
+    }
+}
+
