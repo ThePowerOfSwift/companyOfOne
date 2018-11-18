160f154f59a472e6c73ef8ef1d235e125696ffb8
Jamie Feeny <feeny.jamie@gmail.com>
2018-11-17 19:36:36 -0700

    Working on an alert controller handler.  Doesn't work

---
+++ companyOfOne/Handlers/AlertHandler.swift
@@ -0,0 +1,37 @@
+//
+//  AlertHandler.swift
+//  companyOfOne
+//
+//  Created by Jamie on 2018-11-17.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+import UIKit
+
+class AlertHandler: NSObject {
+//  static let sharedInstance = AlertHandler()
+//    func subCategoryAlert() -> String {
+//        let subCategoryName = String()
+//        let alert = UIAlertController(title: "New SubCategory",
+//                                      message: "Add a new sub category",
+//                                      preferredStyle: .alert)
+//        
+//        let saveAction = UIAlertAction(title: "Save", style: .default) {
+//            [unowned self] action in
+//            guard let textField = alert.textFields?.first,
+//                subCategoryName = textField.text else {
+//                    return
+//            }
+//    }
+//        
+//        let cancelAction = UIAlertAction(title: "Cancel",
+//                                         style: .cancel)
+//        
+//        alert.addTextField()
+//        alert.addAction(saveAction)
+//        alert.addAction(cancelAction)
+//        //present(alert, animated: true)
+//        return subCategoryName
+//}
+//    
+}
