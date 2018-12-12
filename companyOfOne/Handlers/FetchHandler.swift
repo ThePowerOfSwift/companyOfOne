914b4acf25bf526883fe5f1e398b7fb160233f8d
Jamie Feeny <feeny.jamie@gmail.com>
2018-12-12 11:26:56 -0500

    FetchHandler returns proper data discretely for each tab now and the doc tab doesn't include mail or receipts

--- companyOfOne/Handlers/FetchHandler.swift
+++ companyOfOne/Handlers/FetchHandler.swift
@@ -19,7 +19,9 @@ class FetchHandler: NSObject {
         let request =
             NSFetchRequest<NSManagedObject>(entityName: "Document")
         request.sortDescriptors = [NSSortDescriptor(key: "documentDate", ascending: true)]
-        if currentFilter != ""{
+        if currentFilter == "" {
+            request.predicate = NSPredicate(format: "toCategory.name != %@  AND toCategory.name != %@ ", "Mail", "Receipts")
+        }else{
             request.predicate = NSPredicate(format: "toCategory.name == %@", currentFilter)
         }
         ArrayHandler.sharedInstance.documentArray = try! context.fetch(request) as! [Document]
